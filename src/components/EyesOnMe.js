9c67929a38b48111e205a5af8369f1cc9e5fe840
Maral <maral.mamedovna@gmail.com>
2023-06-19 13:10:36 +0300

    Compleed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+ function focusEvent(event) {
+  console.log("Good!");
+ }
+
+ function blurEvent(event) {
+  console.log("Hey! Eyes on me!")
+ }
+
+ return (
+  <div>
+   <button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>
+  </div>
+ );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function Keypad() {
+    function inputHandler(event) {
+        event.target.value
+        console.log(`Entering password...`)
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={inputHandler} />
+        </div>
     )
 }
 
